36e26d77f56e311d90ac5f4f110c29ff6b353612
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2023-09-03 17:44:14 +0000

    feat: rebuilding the project with a diff. approach

--- main.js
+++ main.js
@@ -22,140 +22,165 @@ let beat = 0;
 let intervalId = 0;
 let songId = "";
 let currentAudios = [];
+let test;
 // songs
 songs.forEach((song) => {
-    song.addEventListener("dragstart", (ev) => {
-        songId = ev.target.getAttribute("data-song-id");
-    });
-    song.addEventListener("dragend", (ev) => {
-        var _a;
-        if (((_a = ev.dataTransfer) === null || _a === void 0 ? void 0 : _a.dropEffect) === "copy") {
-            ev.target.draggable = false;
-            ev.target.style.opacity = "0.5";
-        }
-    });
+  song.addEventListener("dragstart", (ev) => {
+    songId = ev.target.getAttribute("data-song-id");
+    ev.target.style.opacity = "0.5";
+  });
+  song.addEventListener("drag", (ev) => {
+    ev.target.style.opacity = "1";
+  });
 });
 // SINGERS
 singers.forEach((singer) => {
-    singer.addEventListener("dragenter", (ev) => {
-        ev.target.classList.add("active");
-    });
-    // singer.addEventListener("pointermove", () => {});
-    // singer.addEventListener("pointerdown", () => {});
-    // singer.addEventListener("pointerup", () => {});
-    // singer.addEventListener("pointerleave", (ev: PointerEvent) => {
-    //   const id = (ev.target as HTMLDivElement).getAttribute("data-song-id")!;
-    //   id && document.querySelector(`audio[data-song-id='${id}']`)
-    //     && handleRemoveAudio(id!)
-    // });
-    singer.addEventListener("dragleave", (ev) => {
-        ev.target.classList.remove("active");
-    });
-    singer.addEventListener("dragover", (ev) => {
-        ev.preventDefault();
-    });
-    singer.addEventListener("click", (ev) => {
-        const id = ev.target.getAttribute("data-song-id");
-        const audio = document.querySelector(`audio[data-song-id='${id}']`);
-        audio.muted = !audio.muted;
-    });
-    singer.addEventListener("drop", (ev) => {
-        var _a;
-        ev.preventDefault();
-        ev.target.setAttribute("data-song-id", songId);
-        (_a = ev.target.lastElementChild) === null || _a === void 0 ? void 0 : _a.setAttribute("data-song-id", songId);
-        ev.target.classList.replace("active", "end");
-        handleAddAudio(songId);
-    });
+  singer.addEventListener("dragenter", (ev) => {
+    ev.target.classList.add("active");
+  });
+  // singer.addEventListener("pointermove", () => {});
+  // singer.addEventListener("pointerdown", () => {});
+  // singer.addEventListener("pointerup", () => {});
+  // singer.addEventListener("pointerleave", (ev: PointerEvent) => {
+  //   const id = (ev.target as HTMLDivElement).getAttribute("data-song-id")!;
+  //   id && document.querySelector(`audio[data-song-id='${id}']`)
+  //     && handleRemoveAudio(id!)
+  // });
+  singer.addEventListener("dragleave", (ev) => {
+    ev.target.classList.remove("active");
+  });
+  singer.addEventListener("dragover", (ev) => {
+    ev.preventDefault();
+  });
+  singer.addEventListener("click", (ev) => {
+    const id = ev.target.getAttribute("data-song-id");
+    const audio = document.querySelector(`audio[data-song-id='${id}']`);
+    audio.muted = !audio.muted;
+  });
+  singer.addEventListener("drop", (ev) => {
+    var _a;
+    ev.preventDefault();
+    if (ev.target.getAttribute("data-song-id") === null) {
+      ev.target.setAttribute("data-song-id", songId);
+      (_a = ev.target.lastElementChild) === null || _a === void 0
+        ? void 0
+        : _a.setAttribute("data-song-id", songId);
+      ev.target.classList.replace("active", "end");
+      const playedSong = document.querySelector(
+        `.song[data-song-id='${songId}'`
+      );
+      playedSong.style.opacity = "0.5";
+      playedSong.draggable = false;
+      handleAddAudio(songId);
+    }
+  });
 });
 function handleRemoveAudio(id) {
-    const audioToRemove = document.querySelector(`audio[data-song-id='${id}']`);
-    audioToRemove.pause();
-    audioToRemove.src = "";
-    audioToRemove.remove();
-    if (!document.getElementsByTagName("audio").length) {
-        beatInterval(true);
-    }
+  const audioToRemove = document.querySelector(`audio[data-song-id='${id}']`);
+  audioToRemove.pause();
+  audioToRemove.src = "";
+  audioToRemove.remove();
+  if (!document.getElementsByTagName("audio").length) {
+    beatInterval(true);
+  }
 }
 function handleAddAudio(id) {
-    var _a;
-    const audio = document.createElement("audio");
-    audio.preload = "auto";
-    audio.loop = true;
-    audio.src = getAudioURl(id);
-    audio.setAttribute("data-song-id", id);
-    // Loader slider unsteady
-    id !== "1" &&
-        ((_a = document
-            .querySelector(`.loader[data-song-id='${id}']`)) === null || _a === void 0 ? void 0 : _a.classList.add("loading"));
-    document.documentElement.style.setProperty("--transition-time", `${beat % 5}s`);
-    document.body.append(audio);
-    currentAudios.push(audio);
-    if (document.getElementsByTagName("audio").length === 1) {
-        audio.play();
-        beatInterval();
-    }
+  var _a;
+  const audio = document.createElement("audio");
+  audio.preload = "auto";
+  audio.loop = true;
+  audio.src = getAudioURl(id);
+  console.log(audio.duration);
+  audio.setAttribute("data-song-id", id);
+  // Loader slider unsteady
+  test = id === "1" ? Math.floor(audio.duration / 2) : 7;
+  id !== "1" &&
+    ((_a = document.querySelector(`.loader[data-song-id='${id}']`)) === null ||
+    _a === void 0
+      ? void 0
+      : _a.classList.add("loading"));
+  document.documentElement.style.setProperty(
+    "--transition-time",
+    `${beat % 5}s`
+  );
+  document.body.append(audio);
+  currentAudios.push(audio);
+  if (document.getElementsByTagName("audio").length === 1) {
+    audio.play();
+    beatInterval();
+  }
 }
 function getAudioURl(id) {
-    switch (+id) {
-        case 1:
-            return "./public/1_atlanta.ogg";
-        case 2:
-            return "./public/2_tuctom.ogg";
-        case 3:
-            return "./public/3_foubreak.ogg";
-        case 4:
-            return "./public/4_koukaki.ogg";
-        case 5:
-            return "./public/5_koungou.ogg";
-        case 6:
-            return "./public/6_bass.ogg";
-        case 7:
-            return "./public/7_monk.ogg";
-        case 8:
-            return "./public/8_sonar.ogg";
-        case 9:
-            return "./public/9_souffle.ogg";
-        case 10:
-            return "./public/10_epifle.ogg";
-        case 11:
-            return "./public/11_arpeg.ogg";
-        case 12:
-            return "./public/12_tromp.ogg";
-        case 13:
-            return "./public/13_pizzi.ogg";
-        case 14:
-            return "./public/14_organ.ogg";
-        case 15:
-            return "./public/15_synth.ogg";
-        case 16:
-            return "./public/16_follow.ogg";
-        case 17:
-            return "./public/17_choir.ogg";
-        case 18:
-            return "./public/18_houhou.ogg";
-        case 19:
-            return "./public/19_reach.ogg";
-        case 20:
-            return "./public/20_believe.ogg";
-        default:
-            return "";
-    }
+  switch (+id) {
+    case 1:
+      return "./public/1_atlanta.ogg";
+    case 2:
+      return "./public/2_tuctom.ogg";
+    case 3:
+      return "./public/3_foubreak.ogg";
+    case 4:
+      return "./public/4_koukaki.ogg";
+    case 5:
+      return "./public/5_koungou.ogg";
+    case 6:
+      return "./public/6_bass.ogg";
+    case 7:
+      return "./public/7_monk.ogg";
+    case 8:
+      return "./public/8_sonar.ogg";
+    case 9:
+      return "./public/9_souffle.ogg";
+    case 10:
+      return "./public/10_epifle.ogg";
+    case 11:
+      return "./public/11_arpeg.ogg";
+    case 12:
+      return "./public/12_tromp.ogg";
+    case 13:
+      return "./public/13_pizzi.ogg";
+    case 14:
+      return "./public/14_organ.ogg";
+    case 15:
+      return "./public/15_synth.ogg";
+    case 16:
+      return "./public/16_follow.ogg";
+    case 17:
+      return "./public/17_choir.ogg";
+    case 18:
+      return "./public/18_houhou.ogg";
+    case 19:
+      return "./public/19_reach.ogg";
+    case 20:
+      return "./public/20_believe.ogg";
+    default:
+      return "";
+  }
 }
 function beatInterval(clear = false) {
-    if (clear) {
-        clearInterval(intervalId);
-    }
-    intervalId = setInterval(() => {
-        beat += 1;
-        beat % 5 === 0 &&
-            currentAudios.forEach((audio, i) => {
-                var _a;
-                (audio === null || audio === void 0 ? void 0 : audio.paused) && (audio === null || audio === void 0 ? void 0 : audio.play());
-                const id = audio.getAttribute("data-song-id");
-                document.documentElement.style.setProperty("--transition-time", `0s`);
-                (_a = document
-                    .querySelector(`.loader[data-song-id='${id}']`)) === null || _a === void 0 ? void 0 : _a.classList.remove("loading");
-            });
-    }, 1000);
+  if (clear) {
+    clearInterval(intervalId);
+  }
+  intervalId = setInterval(() => {
+    beat += 1;
+    beat % test === 0 &&
+      currentAudios.forEach((audio, i) => {
+        var _a;
+        (audio === null || audio === void 0 ? void 0 : audio.paused) &&
+          (audio === null || audio === void 0 ? void 0 : audio.play());
+        const id = audio.getAttribute("data-song-id");
+        document.documentElement.style.setProperty("--transition-time", `0s`);
+        (_a = document.querySelector(`.loader[data-song-id='${id}']`)) ===
+          null || _a === void 0
+          ? void 0
+          : _a.classList.remove("loading");
+      });
+  }, 1000);
 }
+
+// fetch(getAudioURl(1)).then(async (resp) => {
+//   const audioURl = URL.createObjectURL(await resp.blob());
+//   console.log(audioURl);
+//   const audio = document.getElementById("audio");
+//   audio.src = audioURl;
+//   audio.play();
+// });
--- main.ts
+++ main.ts
@@ -1,179 +1,99 @@
-/* 
-Now let see how we'll implement this
+class GlobalState {
+  private ready = false;
+  get isReady() {
+    return this.ready;
+  }
+  set isLoading(loading: boolean) {
+    this.ready = !loading;
+  }
+}
 
-All the audio files will be located in the /public folder, on later implementation they will be fetched lazily optimizing the website
+const GLOBAL = new GlobalState();
 
-We'll have a function for each small features and there'll be event listener on every single piece of the app from the songs to the singers
+// Fetch all audio's and videos beforehand
+const allVideoLinks: { [key: number]: string } = {
+  1: "./public/video1.webm",
+  2: "./public/video2.webm",
+  3: "./public/video3.webm",
+};
 
-**Singers**
-* The container element will have an onDragEnter **event listener(event)** on it - which moves the eyes of the singers toward the moved song
-* Each singer will have an onDrop event on it - Which triggers the changeSrcOfAudio function that controls the song and a class will be added that shows it's active
+const allAudioLinks: { [key: number]: string } = {
+  1: "./public/1_atlanta.ogg",
+  2: "./public/2_tuctom.ogg",
+  3: "./public/3_foubreak.ogg",
+  4: "./public/4_koukaki.ogg",
+  5: "./public/5_koungou.ogg",
+  6: "./public/6_bass.ogg",
+  7: "./public/7_monk.ogg",
+  8: "./public/8_sonar.ogg",
+  9: "./public/9_souffle.ogg",
+  10: "./public/10_epifle.ogg",
+  11: "./public/11_arpeg.ogg",
+  12: "./public/12_tromp.ogg",
+  13: "./public/13_pizzi.ogg",
+  14: "./public/14_organ.ogg",
+  15: "./public/15_synth.ogg",
+  16: "./public/16_follow.ogg",
+  17: "./public/17_choir.ogg",
+  18: "./public/18_houhou.ogg",
+  19: "./public/19_reach.ogg",
+  20: "./public/20_believe.ogg",
+};
+const allCachedAudioURL: {
+  [key: number]: string;
+} = {};
+const allCachedVideoURL: {
+  [key: number]: string;
+} = {};
 
-** songs**
-* The song will be draggable, and the singers will be it's only active drop point, it'll return to it's original position otherwise
-* Each song will have an id that identifies the song it represent
+// fetch blog helper function
+async function fetchBlob(audioLink: string): Promise<Blob> {
+  try {
+    const response = await (await fetch(audioLink)).blob();
+    return response;
+  } catch (err) {
+    throw new Error("Invalid URL");
+  }
+}
 
-MORE TO Come
-*/
-const singers: NodeListOf<HTMLDivElement> =
-  document.querySelectorAll(".singer");
-const songs: NodeListOf<HTMLDivElement> = document.querySelectorAll(".song");
-let beat: number = 0;
-let intervalId: number = 0;
-let songId: string = "";
-let currentAudios: HTMLAudioElement[] = [];
-
-// songs
-songs.forEach((song) => {
-  song.addEventListener("dragstart", (ev: DragEvent) => {
-    songId = (ev.target as HTMLAudioElement).getAttribute("data-song-id")!;
-  });
-  song.addEventListener("dragend", (ev: DragEvent) => {
-    if (ev.dataTransfer?.dropEffect === "copy") {
-      (ev.target as HTMLAudioElement).draggable = false;
-      (ev.target as HTMLAudioElement).style.opacity = "0.5";
+// Fetch all audio and video files into memory
+try {
+  (async function cacheAudioURL() {
+    for (const audioLink in allAudioLinks) {
+      const audioBlob = await fetchBlob(audioLink);
+      allCachedAudioURL[audioLink] = URL.createObjectURL(audioBlob);
     }
-  });
-});
+    console.log(allCachedAudioURL);
+  })();
 
-// SINGERS
-singers.forEach((singer: HTMLDivElement): void => {
-  singer.addEventListener("dragenter", (ev: DragEvent) => {
-    (ev.target as HTMLDivElement).classList.add("active");
-  });
-  // singer.addEventListener("pointermove", () => {});
-  // singer.addEventListener("pointerdown", () => {});
-  // singer.addEventListener("pointerup", () => {});
-  // singer.addEventListener("pointerleave", (ev: PointerEvent) => {
-  //   const id = (ev.target as HTMLDivElement).getAttribute("data-song-id")!;
-  //   id && document.querySelector(`audio[data-song-id='${id}']`)
-  //     && handleRemoveAudio(id!)
-  // });
-  singer.addEventListener("dragleave", (ev: DragEvent) => {
-    (ev.target as HTMLDivElement).classList.remove("active");
-  });
-  singer.addEventListener("dragover", (ev: DragEvent) => {
-    ev.preventDefault();
-  });
-  singer.addEventListener("click", (ev: MouseEvent) => {
-    const id = (ev.target as HTMLDivElement).getAttribute("data-song-id")!;
-    const audio: HTMLAudioElement = document.querySelector(
-      `audio[data-song-id='${id}']`
-    )!;
-    audio.muted = !audio.muted;
-  });
-  singer.addEventListener("drop", (ev: DragEvent) => {
-    ev.preventDefault();
-    if ((ev.target as HTMLDivElement).getAttribute("data-song-id") === null) {
-      (ev.target as HTMLDivElement).setAttribute("data-song-id", songId);
-      (ev.target as HTMLDivElement).lastElementChild?.setAttribute(
-        "data-song-id",
-        songId
-      );
-      (ev.target as HTMLDivElement).classList.replace("active", "end");
-      handleAddAudio(songId);
+  (async function cacheVideoURL() {
+    for (const VideoLink in allVideoLinks) {
+      const VideoBlob = await fetchBlob(VideoLink);
+      allCachedVideoURL[VideoLink] = URL.createObjectURL(VideoBlob);
     }
-  });
-});
+    console.log(allCachedVideoURL);
+  })();
+} catch (err) {
+  throw new Error();
+}
+GLOBAL.isLoading = false;
 
-function handleRemoveAudio(id: string) {
-  const audioToRemove: HTMLAudioElement = document.querySelector(
-    `audio[data-song-id='${id}']`
-  )!;
-  audioToRemove.pause();
-  audioToRemove.src = "";
-  audioToRemove.remove();
-
-  if (!document.getElementsByTagName("audio").length) {
-    beatInterval(true);
-  }
+if (GLOBAL.isReady) {
+  (
+    document.getElementsByClassName("splashscreen")[0] as HTMLDivElement
+  ).style.display = "none";
 }
 
-function handleAddAudio(id: string) {
-  const audio: HTMLAudioElement = document.createElement("audio");
-  audio.preload = "auto";
-  audio.loop = true;
-  audio.src = getAudioURl(id);
-  audio.setAttribute("data-song-id", id);
-  // Loader slider unsteady
-  id !== "1" &&
-    document
-      .querySelector(`.loader[data-song-id='${id}']`)
-      ?.classList.add("loading");
-  document.documentElement.style.setProperty(
-    "--transition-time",
-    `${beat % 5}s`
-  );
-  document.body.append(audio);
-  currentAudios.push(audio);
-  if (document.getElementsByTagName("audio").length === 1) {
-    audio.play();
-    beatInterval();
-  }
-}
+// dragging songs
 
-function getAudioURl(id: string) {
-  switch (+id) {
-    case 1:
-      return "./public/1_atlanta.ogg";
-    case 2:
-      return "./public/2_tuctom.ogg";
-    case 3:
-      return "./public/3_foubreak.ogg";
-    case 4:
-      return "./public/4_koukaki.ogg";
-    case 5:
-      return "./public/5_koungou.ogg";
-    case 6:
-      return "./public/6_bass.ogg";
-    case 7:
-      return "./public/7_monk.ogg";
-    case 8:
-      return "./public/8_sonar.ogg";
-    case 9:
-      return "./public/9_souffle.ogg";
-    case 10:
-      return "./public/10_epifle.ogg";
-    case 11:
-      return "./public/11_arpeg.ogg";
-    case 12:
-      return "./public/12_tromp.ogg";
-    case 13:
-      return "./public/13_pizzi.ogg";
-    case 14:
-      return "./public/14_organ.ogg";
-    case 15:
-      return "./public/15_synth.ogg";
-    case 16:
-      return "./public/16_follow.ogg";
-    case 17:
-      return "./public/17_choir.ogg";
-    case 18:
-      return "./public/18_houhou.ogg";
-    case 19:
-      return "./public/19_reach.ogg";
-    case 20:
-      return "./public/20_believe.ogg";
+// customizing the singers
 
-    default:
-      return "";
-  }
-}
+// Playing video
 
-function beatInterval(clear: boolean = false) {
-  if (clear) {
-    clearInterval(intervalId);
-  }
-  intervalId = setInterval(() => {
-    beat += 1;
-    beat % 5 === 0 &&
-      currentAudios.forEach((audio: HTMLAudioElement, i) => {
-        audio?.paused && audio?.play();
-        const id = audio.getAttribute("data-song-id");
-        document.documentElement.style.setProperty("--transition-time", `0s`);
-        document
-          .querySelector(`.loader[data-song-id='${id}']`)
-          ?.classList.remove("loading");
-      });
-  }, 1000);
-}
+// Playing audio
+
+// Muting audio
+
+// Removing audio
+
+// Animations
