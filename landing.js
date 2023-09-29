ed3a8a75d63f9486ec93261fd2a7c278c754128a
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2023-09-29 15:28:25 +0000

    feat: worked more on the landing page

--- landing.js
+++ landing.js
@@ -7,16 +7,97 @@ const choose = document.getElementById("choose");
 const versions = [...document.getElementsByClassName("version")];
 const info = document.getElementById("info");
 const settings = document.getElementById("settings");
+const modal = document.getElementById("modal");
+const tabButtons = [
+    ...document.getElementsByClassName("tab-button"),
+];
+const pops = [...document.getElementsByClassName("pop")];
+const navl = document.getElementById("navl");
+const navr = document.getElementById("navr");
+let progress = 0;
+const prog = [...document.getElementsByClassName("prog")];
+const slide = document.getElementById("slide");
 window.onload = () => {
     if (screen.orientation.type.includes("landscape")) {
         introAnim();
     }
 };
+navl.onclick = moveTutLeft;
+navr.onclick = moveTutRight;
+function moveTutRight() {
+    navl.classList.remove("disable");
+    progress = ++progress % 5;
+    if (progress <= 0) {
+        navl.classList.add("disable");
+        progress = 0;
+    }
+    prog.forEach((element, i) => {
+        if (i === progress) {
+            element.classList.add("active");
+        }
+        else {
+            element.classList.remove("active");
+        }
+    });
+    slide.style.transform = `translate(-${progress * 20}%, 0%)`;
+    console.log(progress);
+}
+function moveTutLeft() {
+    console.log("fuck");
+    if (!(progress <= 0)) {
+        progress = --progress % 5;
+    }
+    else {
+        navl.classList.add("disable");
+        progress = 0;
+    }
+    prog.forEach((element, i) => {
+        if (i === progress) {
+            element.classList.add("active");
+        }
+        else {
+            element.classList.remove("active");
+        }
+    });
+    slide.style.transform = `translate(${progress * 20}%, 0%)`;
+    console.log(progress);
+}
 window.addEventListener("orientationchange", () => {
     if (screen.orientation.type.includes("landscape")) {
         introAnim();
     }
 });
+info.onclick = () => {
+    if (!modal.classList.contains("animateFadeIn")) {
+        modal.classList.add("animateFadeIn");
+        tabButtons[0].classList.add("active");
+        pops[0].classList.add("active");
+    }
+};
+tabButtons.forEach((tabButton) => {
+    tabButton.onclick = (ev) => {
+        const target = ev.target;
+        tabButtons.forEach((tab, i) => {
+            console.log(ev.target);
+            if (tab.id === target.id ||
+                (target.parentElement.classList.contains("tab-button") &&
+                    tab.id === target.parentElement.id)) {
+                tab.classList.add("active");
+                pops[i].classList.add("active");
+            }
+            else {
+                tab.classList.remove("active");
+                pops[i].classList.remove("active");
+            }
+        });
+    };
+});
+modal.onclick = (ev) => {
+    if (ev.target.id === "modal") {
+        modal.classList.remove("animateFadeIn");
+        pops.forEach((pop) => pop.classList.remove("active"));
+    }
+};
 function introAnim() {
     setTimeout(() => {
         title.classList.add("titleUp");
@@ -29,7 +110,7 @@ function introAnim() {
         title.classList.add("titleUpHide");
         experience.classList.add("experienceUpHide");
         headphone.classList.add("headphoneUpHide");
-        choose.classList.add("chooseUpHide");
+        settings.classList.add("experienceUp");
         versions.forEach((version, i) => {
             setTimeout(() => {
                 version.classList.add("versionUp");
--- landing.ts
+++ landing.ts
@@ -3,21 +3,105 @@ const pump = document.getElementById("pump") as HTMLDivElement;
 const experience = document.getElementById("experience") as HTMLDivElement;
 const headphone = document.getElementById("headphone") as HTMLDivElement;
 const choose = document.getElementById("choose") as HTMLDivElement;
-const versions = [...document.getElementsByClassName("version")]
-const info = document.getElementById("info") as HTMLDivElement
-const settings = document.getElementById("settings") as HTMLDivElement
+const versions = [...document.getElementsByClassName("version")];
+const info = document.getElementById("info") as HTMLDivElement;
+const settings = document.getElementById("settings") as HTMLDivElement;
+const modal = document.getElementById("modal") as HTMLDivElement;
+const tabButtons = [
+  ...document.getElementsByClassName("tab-button"),
+] as HTMLDivElement[];
+const pops = [...document.getElementsByClassName("pop")] as HTMLDivElement[];
+const navl = document.getElementById("navl") as HTMLDivElement;
+const navr = document.getElementById("navr") as HTMLDivElement;
+let progress = 0;
+const prog = [...document.getElementsByClassName("prog")] as HTMLDivElement[];
+const slide = document.getElementById("slide") as HTMLDivElement;
 
 window.onload = () => {
-    if(screen.orientation.type.includes("landscape")) {
-        introAnim()
-    }
+  if (screen.orientation.type.includes("landscape")) {
+    introAnim();
+  }
 };
 
-window.addEventListener("orientationchange", () => {
-    if (screen.orientation.type.includes("landscape")) {
-      introAnim();
+navl.onclick = moveTutLeft;
+navr.onclick = moveTutRight;
+
+function moveTutRight() {
+  navl.classList.remove("disable");
+  progress = ++progress % 5;
+  if (progress <= 0) {
+    navl.classList.add("disable");
+    progress = 0;
+  }
+  prog.forEach((element, i) => {
+    if (i === progress) {
+      element.classList.add("active");
+    } else {
+      element.classList.remove("active");
     }
-})
+  });
+  slide.style.transform = `translate(-${progress * 20}%, 0%)`;
+  console.log(progress);
+}
+function moveTutLeft() {
+  console.log("fuck");
+  if (!(progress <= 0)) {
+    progress = --progress % 5;
+  } else {
+    navl.classList.add("disable");
+    progress = 0;
+  }
+  prog.forEach((element, i) => {
+    if (i === progress) {
+      element.classList.add("active");
+    } else {
+      element.classList.remove("active");
+    }
+  });
+  slide.style.transform = `translate(${progress * 20}%, 0%)`;
+  console.log(progress);
+}
+
+window.addEventListener("orientationchange", () => {
+  if (screen.orientation.type.includes("landscape")) {
+    introAnim();
+  }
+});
+
+info.onclick = () => {
+  if (!modal.classList.contains("animateFadeIn")) {
+    modal.classList.add("animateFadeIn");
+    tabButtons[0].classList.add("active");
+    pops[0].classList.add("active");
+  }
+};
+
+tabButtons.forEach((tabButton) => {
+  tabButton.onclick = (ev: MouseEvent) => {
+    const target = ev.target as HTMLDivElement;
+    tabButtons.forEach((tab, i) => {
+      console.log(ev.target);
+      if (
+        tab.id === target.id ||
+        (target.parentElement!.classList.contains("tab-button") &&
+          tab.id === target.parentElement!.id)
+      ) {
+        tab.classList.add("active");
+        pops[i].classList.add("active");
+      } else {
+        tab.classList.remove("active");
+        pops[i].classList.remove("active");
+      }
+    });
+  };
+});
+
+modal.onclick = (ev: MouseEvent) => {
+  if ((ev.target as HTMLDivElement).id === "modal") {
+    modal.classList.remove("animateFadeIn");
+    pops.forEach((pop) => pop.classList.remove("active"));
+  }
+};
 
 function introAnim() {
   setTimeout(() => {
@@ -31,11 +115,11 @@ function introAnim() {
     title.classList.add("titleUpHide");
     experience.classList.add("experienceUpHide");
     headphone.classList.add("headphoneUpHide");
-    choose.classList.add("chooseUpHide");
+    settings.classList.add("experienceUp");
     versions.forEach((version, i) => {
-        setTimeout(() => {
-            version.classList.add("versionUp")
-        }, i * 50);
-    })
+      setTimeout(() => {
+        version.classList.add("versionUp");
+      }, i * 50);
+    });
   }, 5000);
 }
