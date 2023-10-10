61559e1518436b0b5f33eab4dedb37124afbf5f3
Tobiloba Adebisi <oluwatobilobaadebisi6@gmail.com>
2023-10-10 17:37:37 +0000

    feat: quick fix

--- general.ts
+++ general.ts
@@ -26,9 +26,7 @@ fullscreen.onclick = () => {
 };
 
 window.addEventListener("load", () => {
-  if (screen.orientation.type.includes("portrait")) {
-    orientationPop.classList.add("active")
-  }
+    orientationPop.classList.add("active");
 });
 screen.orientation.addEventListener("change", () => {
   if (screen.orientation.type.includes("landscape")) {
